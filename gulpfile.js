ef50a17dc586c3d27d6a2b4da48a6d59b7405cc1
jjq <cz_jjq@163.com>
2017-05-18 18:03:20 +0800

    upgrade

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var package = require('./package.json');
 
-//要点：Stream 转换
+//解决browserify + gulp 的注意点 ： Stream 转换
 //vinyl-source-stream + vinyl-buffer
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
@@ -18,7 +18,7 @@ var paths = {
 };
 
 gulp.task("build", function(){
-    var b = browserify("./app/main.js",browserifyOpts);
+    var b = browserify(browserifyOpts);
     //注意：这里使用gulp.dest()输出时会出现问题，使用process.stdout输出则不会，测试后加入source和buffer急性转换即可
     return b.bundle()
         .pipe(source('common.js')) // gives streaming vinyl file object
