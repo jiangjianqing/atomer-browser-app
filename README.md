662bc443528360bb45886f5429ea0127250728f6
cz_jjq <cz_jjq@163.com>
2017-05-21 08:35:34 +0800

    upgrade

--- README.md
+++ README.md
@@ -15,3 +15,14 @@ browserify -r assert -r events -r querystring -r stream -r util -r buffer ./app/
 ##使用以下第三方库作为基础功能模块
 handlebars  --  模板处理
 vue -- view处理，类似还有react，但是vue感觉更接地气
+
+#### Auto--test
+测试使用了karma+mocha的组合
+
+karma方便在浏览器端进行调试 -- 通过karma start 启动
+```shell
+但要先安装karma的命令行
+npm install -g karma-cli
+```
+mocha则集成进命令行 -- 通过npm test 启动
+上述两种方法组合使用
--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,83 @@
 // Karma configuration
 // Generated on Sun May 21 2017 07:19:27 GMT+0800 (CST)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-      //20170521 重要: include browserify first in used frameworks
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['browserify', 'mocha'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      'test/**/*Spec.js'
-    ],
+        //让karma+mocha+browserify成功配置的关键，请查阅https://github.com/nikku/karma-browserify
+        //20170521 重要: include browserify first in used frameworks
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['browserify', 'mocha'],
 
 
-    // list of files to exclude
-    exclude: [
-      '**/*.swp'
-    ],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-        'test/**/*.js': [ 'browserify' ]
-    },
+        // list of files / patterns to load in the browser
+        files: [
+            'test/**/*Spec.js'
+        ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        // list of files to exclude
+        exclude: [
+            '**/*.swp'
+        ],
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'test/**/*.js': ['browserify']
+        },
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Firefox', 'Chrome'],
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  });
-}
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity,
+
+        // add additional browserify configuration properties here
+        // such as transform and/or debug=true to generate source maps
+        browserify: {
+            debug: true,
+            transform: ['brfs', "browserify-shim"]
+        },
+
+        watchify: {
+            poll: true
+        }
+    });
+};
