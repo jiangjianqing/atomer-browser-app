6b99304283478eb75e2530c4dbe580cbf90ca6bd
cz_jjq <cz_jjq@163.com>
2017-05-22 23:01:25 +0800

    upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,6 @@
  */
 var path = require('path');
 var webpack = require('webpack');
-var fs = require('fs');
-
-var babelConfig = JSON.parse(fs.readFileSync('./.babelrc'));
 
 module.exports = {
     //2、进出口文件配置
@@ -21,14 +18,11 @@ module.exports = {
     },
     module: {//在配置文件里添加加载器说明，指明每种文件需要什么加载器处理
         loaders: [
-            {
+            {//20170522:经测试，多个同样的测试放在一起最ok
                 test: /\.js$/,
                 exclude:/node_modules/,
                 loader: [ //多个loader一起使用
-                    {
-                        loader:'babel-loader',
-                        query:babelConfig
-                    },
+                    'babel-loader', //es2015处理
                     //重要：结合使用webpack和browserify的transform
                     "transform-loader?brfs",
                     "transform-loader?browserify-shim"
@@ -38,16 +32,11 @@ module.exports = {
                 test: /\.json$/,
                 loader: "json-loader"
             },
-            /* 20170522 暂时都放在上面同一个loader中测试一下是否可行
-            {//5、编译es6配置
-                test:/\.js$/,
-                exclude:/node_modules/,
-                loader:'babel-loader',//在webpack的module部分的loaders里进行配置即可
-                query:babelConfig
-            },*/
+             //20170522 暂时都放在上面同一个loader中测试一下是否可行
+
             {//3、CSS-loader
                 test:/\.css$/,
-                loader:'style-loader!css-loader'//添加对样式表的处理
+                loader:'style-loader!css-loader'//添加对样式表的处理,感叹号的作用在于使同一文件能够使用不同类型的loader
             }
 
         ]
@@ -78,12 +67,16 @@ module.exports = {
 
     plugins:[],//插件
 
+    //使用eval打包源文件模块，在同一个文件中生成干净的完整的source map。
+    //这个选项可以在不影响构建速度的前提下生成完整的sourcemap，但是对打包后输出的JS文件的执行具有性能和安全的隐患。
+    // 不过在开发阶段这是一个非常好的选项.
     //https://webpack.github.io/docs/configuration.html#devtool
     devtool: '#eval-source-map'
 };
 
 //如果是产品模式则设定一些
 if (process.env.NODE_ENV === 'production') {
+    //在一个单独的文件中产生一个完整且功能完全的文件。这个文件具有最好的source map，但是它会减慢打包文件的构建速度；
     module.exports.devtool = '#source-map';
     // http://vue-loader.vuejs.org/en/workflow/production.html
 
@@ -92,7 +85,9 @@ if (process.env.NODE_ENV === 'production') {
             'process.env': {
                 NODE_ENV: '"production"'
             }
-        })
+        })//,
+        //new webpack.optimize.UglifyJsPlugin()
+        //new ExtractTextPlugin("[name]-[hash].css")
     ]);
     /*
     module.exports.plugins = (module.exports.plugins || []).concat([
