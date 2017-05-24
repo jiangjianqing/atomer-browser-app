a2ec454b6c7ef580126c7a0c06714bf140706389
jjq <cz_jjq@163.com>
2017-05-24 17:08:37 +0800

    upgrade

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -6,19 +6,13 @@
         <v-link href="/about">About</v-link>
       </li>
     </ul>
-    <table class="easyui-datagrid" title="Basic DataGrid" style="width:700px;height:250px"
-           data-options="singleSelect:true,collapsible:true,url:'datagrid_data1.json',method:'get'">
-      <thead>
-      <tr>
-        <th data-options="field:'itemid',width:80">Item ID</th>
-        <th data-options="field:'productid',width:100">Product</th>
-        <th data-options="field:'listprice',width:80,align:'right'">List Price</th>
-        <th data-options="field:'unitcost',width:80,align:'right'">Unit Cost</th>
-        <th data-options="field:'attr1',width:250">Attribute</th>
-        <th data-options="field:'status',width:60,align:'center'">Status</th>
-      </tr>
-      </thead>
-    </table>
+    <ul class="list-group">
+      <li class="list-group-item">Cras justo odio</li>
+      <li class="list-group-item">Dapibus ac facilisis in</li>
+      <li class="list-group-item">Morbi leo risus</li>
+      <li class="list-group-item">Porta ac consectetur ac</li>
+      <li class="list-group-item">Vestibulum at eros</li>
+    </ul>
     <slot></slot>
   </div>
 </template>
