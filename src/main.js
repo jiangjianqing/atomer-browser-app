05744193c350a3199ef0a06b59e0f221f189ae17
jjq <cz_jjq@163.com>
2017-05-25 10:51:51 +0800

    upgrade

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * Created by jjq on 5/23/17.
  */
 let Vue = require('vue').default; //20170523 : vue2.x需要这样使用
-//20170525 重要: page和director都是路由组件,page的介绍中可以学到一些route知识,director可以动态定义路由，各有优缺点
+//20170525 重要: page和director都是路由组件,page的介绍中可以学到一些route知识,director可以动态定义路由(Adhoc Routing 和 Scoped Routing)，各有优缺点
 //20170525 :经过测试 ,director 1.2.6在使用时不会拦截页面向服务器请求数据，暂时还是用page
 //When developing large client-side or server-side applications it is not always possible to define routes in one location.
 // Usually individual decoupled components register their own routes with the application router.
