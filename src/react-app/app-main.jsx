9033fd3df69b4c547dec3558f523a34e76efcdd8
jjq <cz_jjq@163.com>
2017-06-07 19:06:10 +0800

    upgrade

--- src/react-app/app-main.jsx
+++ src/react-app/app-main.jsx
@@ -10,9 +10,12 @@ let components = {
 class AppMain extends React.Component {
     constructor(props){
         super(props);
+        this.counter = 0;
+        console.log("lifecycle范例基本都写在 user-manage 中，请查阅。");
     }
 
     render() {
+        console.log("app-main测试状态保持(keep-alive) : " + this.counter++);
         return (
             <div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
                 {components[this.props.currentComponent]}
--- src/react-app/components/user-manage.jsx
+++ src/react-app/components/user-manage.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 
 class UserManage extends React.Component {
+    componentDidMount() {
+        console.log("user-manage mounted!");
+    }
+
+    componentWillUnmount() {
+        console.log("user-manage unmounted!");
+    }
+
+    //If you need to update state in response to a prop change, use componentWillReceiveProps() instead.
+
+
+//shouldComponentUpdate() // deprecated!!
+    //Note that in the future React may treat shouldComponentUpdate() as a hint rather than a strict directive,
+    // and returning false may still result in a re-rendering of the component.
+
+
     render() {
         return (
             <div>
