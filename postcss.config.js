31040fd35ab3e27e17157c66d9128384b6e1077c
jjq <cz_jjq@163.com>
2017-06-12 18:40:34 +0800

    use ctx var

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-module.exports = ({ file, options, env }) => ({
-    map: 'inline',
-    parser: file.extname === '.sss' ? 'sugarss' :  false,
+module.exports = (ctx) => ({
+    map: ctx.options.map,
+    parser: ctx.file.extname === '.sss' ? 'sugarss' :  false,
     plugins: {
-        'postcss-import': { root: file.dirname },
-        'postcss-cssnext': options.cssnext ? options.cssnext : false,
-        'cssnano': env === 'production' ? options.cssnano : false //css压缩
+        'postcss-import': { root: ctx.file.dirname },
+        'postcss-cssnext': ctx.options.cssnext ? ctx.options.cssnext : true,
+        'cssnano': ctx.env === 'production' ? ctx.options.cssnano : false //css压缩
     }
 })
